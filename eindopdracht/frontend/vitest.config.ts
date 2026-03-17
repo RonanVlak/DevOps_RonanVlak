b31c97f23b227ad6fff88baf47e34c7dc3810f3e
Ronan Vlak <71377825+RonanVlak@users.noreply.github.com>
2026-03-17 14:16:08 +0100

    Updated vitest files

--- eindopdracht/frontend/vitest.config.ts
+++ eindopdracht/frontend/vitest.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vitest/config';
 import angular from '@analogjs/vitest-angular';
 
 export default defineConfig({
-  plugins: [angular()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/test-setup.ts'],
     include: ['**/*.spec.ts'],
+    // Voeg dit toe om ESM problemen te voorkomen:
+    server: {
+      deps: {
+        inline: ['@analogjs/vitest-angular']
+      }
+    }
   },
+  plugins: [angular()],
 });
